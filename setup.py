1b19558096b99a44ba339c6e10fd8bd469e67ded
BardiaNikpour <157668628+BardiaNikpour@users.noreply.github.com>
2024-03-26 23:02:17 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/BardiaNikpour/Module9',
+    author='Bardia Nikpour',
+    author_email='ddb4t4@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
